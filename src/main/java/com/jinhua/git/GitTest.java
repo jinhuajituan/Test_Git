c871e322dd191b75f7867ac21edc3e6bba315fcb
AFSFDS <2066131624@qq.com>
2022-11-17 15:12:37 +0800

    second commit

--- src/main/java/com/jinhua/git/GitTest.java
+++ src/main/java/com/jinhua/git/GitTest.java
@@ -3,5 +3,6 @@ package com.jinhua.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
